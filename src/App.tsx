2b6a37e611e5ff9c13856d4accbdcbec7ca261f5
namkijoo <rlwn5904@naver.com>
2025-02-26 16:57:53 +0900

    svg처리

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,12 @@ function App() {
     <div ref={rateRef}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+            crossOrigin="anonymous"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
